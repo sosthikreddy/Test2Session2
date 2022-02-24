7a80daed0acabb4e4386e2774cb2ca449bf69d88
Sosthik R <96185139+sosthikreddy@users.noreply.github.com>
2022-02-24 14:34:52 +0530

    Update p2final.c
    ...

--- p2final.c
+++ p2final.c
@@ -1,1 +1,40 @@
 
+#include<stdio.h>
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3)
+{
+  printf("enter the valus for the x1,y1,x2,y2,x3,y3\n");
+  scanf("%f %f %f %f %f %f ",x1,y1,x2,y2,x3,y3);
+}
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
+{
+  float area;
+    area=0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2));
+     if (area==0)
+     {
+       return 1;
+     }
+  else
+     {
+       return 2;
+     }
+}
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int istriangle)
+{
+if(istriangle==1)
+{
+  printf("it is not triangle");
+}
+  if(istriangle==2)
+  {
+    printf("it is a tiangle ");
+  }
+}
+float main()
+{
+  float x1,y1,x2,y2,x3,y3;
+  float area;
+  input_triangle(&x1,&y1,&x2,&y2,&x3,&y3);
+  area=is_triangle( x1,y1,x2,y2,x3,y3);
+  output(x1,y1,x2,y2,x3,y3,area);
+  return 0;
+}
